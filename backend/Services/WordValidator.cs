272c5fd153ba117e0f8c5024ba64a284a3d9b754
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-02 15:01:46 +0200

    Added the function to validate words letters

--- backend/Services/WordValidator.cs
+++ backend/Services/WordValidator.cs
@@ -1,12 +1,22 @@
+using System.Text.RegularExpressions;
 namespace WordMaster.Services;
 
 public class WordValidator
 {
+  // validates that the word is at least 2 characters long 
   public bool IsValidLength(string word)
   {
     if (string.IsNullOrWhiteSpace(word))
       return false;
 
     return word.Length >= 2;
+  }
+  // validates that the word does not contain numbers or symbols or whitespace or anything other than (a-z, å, ä, ö)
+  public bool IsValidCharacters(string word)
+  {
+    if (string.IsNullOrWhiteSpace(word))
+      return false;
+
+    return Regex.IsMatch(word, "^[a-zA-ZåäöÅÄÖ]+$");
   }
 }
