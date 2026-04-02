b302eafb2aa537ceb2812811553434f8f2e52c88
arbazshah52 <arbazhussain067@gmail.com>
2026-04-02 13:02:35 +0200

    landingpage star

--- frontend/pages/Landingpage.tsx
+++ frontend/pages/Landingpage.tsx
@@ -3,4 +3,18 @@ import './Landingpage.css'
 import type { ModalType, StarData, CreateModalProps, JoinModalProps } from '../interfaces/landing';
 
 const Stars: React.FC = () => {
-  const stars = useRef<StarData[]>([])
+    const stars = useRef<StarData[]>([])
+if (!stars.current.length) {
+    for (let i = 0; i < 60; i++) {
+      const size = Math.random() * 2 + 0.5
+      stars.current.push({
+        id: i,
+        left: Math.random() * 100,
+        top: Math.random() * 40,
+        size,
+        d:   (2 + Math.random() * 4).toFixed(1) + 's',
+        del: (Math.random() * 5).toFixed(1) + 's',
+        min: (0.2 + Math.random() * 0.3).toFixed(2),
+      })
+    }
+  }
