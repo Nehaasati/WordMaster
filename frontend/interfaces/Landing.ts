cdb8459cc82e44f5bdf695bd03853bf1d468e135
arbazshah52 <arbazhussain067@gmail.com>
2026-04-02 12:55:57 +0200

    number

--- frontend/interfaces/Landing.ts
+++ frontend/interfaces/Landing.ts
@@ -1,1 +1,10 @@
 export type ModalType = 'create' | 'join' | null;
+export interface StarData {
+  id: number
+  left: number
+  top: number
+  size: number
+  d: string
+  del: string
+  min: string
+}
