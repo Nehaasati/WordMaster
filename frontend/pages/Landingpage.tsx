8f913a8305d572bf78a6a06766fc9950c7955fec
arbazshah52 <arbazhussain067@gmail.com>
2026-04-02 13:01:32 +0200

    landingpage const

--- frontend/pages/Landingpage.tsx
+++ frontend/pages/Landingpage.tsx
@@ -1,3 +1,6 @@
 import React, { useState, useRef } from 'react'
 import './Landingpage.css'
-import type { ModalType, StarData, CreateModalProps, JoinModalProps } from '../interfaces/landing'
+import type { ModalType, StarData, CreateModalProps, JoinModalProps } from '../interfaces/landing';
+
+const Stars: React.FC = () => {
+  const stars = useRef<StarData[]>([])
