a6041d58ab3aea63aeffea0bb516b356ccfa1f9b
arbazshah52 <arbazhussain067@gmail.com>
2026-04-03 23:28:39 +0200

    interface star

--- frontend/interfaces/Gamepage.ts
+++ frontend/interfaces/Gamepage.ts
@@ -9,3 +9,12 @@ export interface CategoryData {
   valid: boolean
   feedback: string
 }
+export interface StarData {
+  id: number
+  left: number
+  top: number
+  size: number
+  d: string
+  del: string
+  min: string
+}
