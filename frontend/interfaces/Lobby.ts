348ed5b521874ded6ca66bc9bcee394f8d39c926
arbazshah52 <arbazhussain067@gmail.com>
2026-04-03 20:26:26 +0200

    interface create looby and join lobby

--- frontend/interfaces/Lobby.ts
+++ frontend/interfaces/Lobby.ts
@@ -2,3 +2,11 @@ export interface LobbyState {
   lobbyId: string
   status: 'waiting' | 'ready' | 'started'
 }
+export interface CreateLobbyResponse {
+  lobbyId: string
+  playerToken: string
+  status: string
+}
+export interface JoinLobbyProps {
+  lobbyId: string
+}
