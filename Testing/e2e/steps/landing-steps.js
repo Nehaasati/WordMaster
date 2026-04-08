566559e33ef56c2b98ba5323cbedc1518cb9d3e0
arbazshah52 <arbazhussain067@gmail.com>
2026-04-08 12:49:22 +0200

    when lobby async,lobby page

--- Testing/e2e/steps/landing-steps.js
+++ Testing/e2e/steps/landing-steps.js
@@ -20,3 +20,11 @@ Then('I should see the Create a lobby button', async ({ page }) => {
 Then('I should see the Join a lobby button', async ({ page }) => {
   await expect(page.getByTestId('btn-join')).toBeVisible();
 });
+When('I click Create a lobby', async ({ page }) => {
+  const game = new GamePage(page);
+  await game.clickCreateLobby();
+});
+ 
+Then('I should see the lobby page', async ({ page }) => {
+  await expect(page.getByTestId('lobby-page')).toBeVisible();
+});
