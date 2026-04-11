a042edb4208fa5a586d4f44b1229597e03d08529
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-11 11:56:27 +0200

    Deleted EnterNamePage.tsx because I used Arbaz modal in order to unite userExperience

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom'
 import LandingPage from '../pages/Landingpage'
 import GamePage from '../pages/GamePage'
 import LobbyPage from '../pages/LobbyPage'
-import EnterNamePage from '../pages/EnterNamePage'
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <Route path="/lobby/:lobbyId" element={<LobbyPage />} />
       <Route path="/game" element={<GamePage />} />
       <Route path="/game/:lobbyId" element={<GamePage />} />
-      <Route path="/enter-name" element={<EnterNamePage />} />
     </Routes>
   );
 }
