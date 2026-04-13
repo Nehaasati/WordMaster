3f09f33c1faaf4c1b2b856a47487e0f00966d9e3
arbazshah52 <arbazhussain067@gmail.com>
2026-04-14 00:35:19 +0200

    buy request and shops response

--- frontend/interfaces/Shop.ts
+++ frontend/interfaces/Shop.ts
@@ -9,3 +9,9 @@ export interface BuyRequest {
   itemId: string
   cost: number
 }
+export interface ShopProps {
+  score: number
+  lobbyId?: string
+  onBuyLetter: (letter: string, cost: number) => void
+  onBuyPowerup: (powerup: string, cost: number) => void
+}
