78fe9f7d23abdc94bd0db805079bc905582531cf
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-14 12:47:09 +0200

    lobby.page

--- Testing/e2e/pages/lobby.page.js
+++ Testing/e2e/pages/lobby.page.js
@@ -1,1 +1,40 @@
 // lobby page
+
+class LobbyPage {
+  constructor(page) {
+    this.page = page;
+    this.readyBtn = '.ready-btn';
+    this.playerBoxes = '.player-box';
+    this.addBotBtn = 'text=+ Lägg till motståndare';
+    this.startBtn = 'text=Starta spelet';
+    this.leaveLobbyBtn = 'text=Lämna lobbyn';
+  }
+
+  async waitForLobby() {
+    await this.page.waitForSelector('.players-list');
+  }
+
+  async getPlayers() {
+    return await this.page.$$eval(this.playerBoxes, boxes =>
+      boxes.map(b => b.textContent.trim())
+    );
+  }
+
+  async clickReady() {
+    await this.page.click(this.readyBtn);
+  }
+
+  async clickStart() {
+    await this.page.click(this.startBtn);
+  }
+
+  async addBot() {
+    await this.page.click(this.addBotBtn);
+  }
+
+  async leaveLobby() {
+    await this.page.click(this.leaveLobbyBtn);
+  }
+}
+
+module.exports = { LobbyPage };
