c2b3718188feddfcb3341de49b6f8a45ef9bde88
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-14 12:56:58 +0200

    Added game page test and lobby page test

--- Testing/e2e/pages/lobby.page.js
+++ Testing/e2e/pages/lobby.page.js
@@ -3,21 +3,55 @@
 class LobbyPage {
   constructor(page) {
     this.page = page;
-    this.readyBtn = '.ready-btn';
-    this.playerBoxes = '.player-box';
+
+    // Player info
+    this.playersList = '.players-list .player-box';
+
+    // Character carousel
+    this.prevBtn = '.ch-arrow >> nth=0';
+    this.nextBtn = '.ch-arrow >> nth=1';
+    this.characterName = '.character-name';
+    this.characterDescription = '.character-description';
+
+    // Game mode
+    this.standardModeBtn = 'text=Standard WordMaster';
+    this.blitzModeBtn = 'text=Blitz WordMaster';
+
+    // Buttons
     this.addBotBtn = 'text=+ Lägg till motståndare';
+    this.readyBtn = '.ready-btn';
     this.startBtn = 'text=Starta spelet';
-    this.leaveLobbyBtn = 'text=Lämna lobbyn';
+    this.copyLinkBtn = 'text=KOPIERA INBJUDNINGSLÄNK';
   }
 
   async waitForLobby() {
-    await this.page.waitForSelector('.players-list');
+    await this.page.waitForSelector('text=VÄLJ EN KARAKTÄR');
   }
 
   async getPlayers() {
-    return await this.page.$$eval(this.playerBoxes, boxes =>
+    return await this.page.$$eval(this.playersList, boxes =>
       boxes.map(b => b.textContent.trim())
     );
+  }
+
+  async selectNextCharacter() {
+    await this.page.click(this.nextBtn);
+  }
+
+  async selectPrevCharacter() {
+    await this.page.click(this.prevBtn);
+  }
+
+  async chooseStandardMode() {
+    await this.page.click(this.standardModeBtn);
+  }
+
+  async chooseBlitzMode() {
+    await this.page.click(this.blitzModeBtn);
+  }
+
+  async addBot() {
+    await this.page.click(this.addBotBtn);
   }
 
   async clickReady() {
@@ -26,14 +60,6 @@ class LobbyPage {
 
   async clickStart() {
     await this.page.click(this.startBtn);
-  }
-
-  async addBot() {
-    await this.page.click(this.addBotBtn);
-  }
-
-  async leaveLobby() {
-    await this.page.click(this.leaveLobbyBtn);
   }
 }
 
