e7dcb0d218f57d8eaa68c1adf3a231247d5315a0
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-14 12:45:53 +0200

    landing page

--- Testing/e2e/pages/landing.page.js
+++ Testing/e2e/pages/landing.page.js
@@ -1,1 +1,33 @@
 // landing page
+
+class LandingPage {
+  constructor(page) {
+    this.page = page;
+    this.createLobbyBtn = 'text=SKAPA EN LOBBY';
+    this.joinLobbyBtn = 'text=GÅ MED I EN LOBBY';
+    this.nameInput = '.wm-modal-input';
+    this.confirmBtn = '.wm-modal-btn--confirm';
+  }
+
+  async goto() {
+    await this.page.goto('http://localhost:5173');
+  }
+
+  async openCreateLobbyModal() {
+    await this.page.click(this.createLobbyBtn);
+  }
+
+  async openJoinLobbyModal() {
+    await this.page.click(this.joinLobbyBtn);
+  }
+
+  async enterName(name) {
+    await this.page.fill(this.nameInput, name);
+  }
+
+  async confirm() {
+    await this.page.click(this.confirmBtn);
+  }
+}
+
+module.exports = { LandingPage };
