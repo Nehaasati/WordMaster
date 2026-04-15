b4d5a31eae2edc65dfec76111684d8553192412f
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-15 20:55:24 +0200

    Trying to solve the interface default

--- frontend/interfaces/Gamepage.ts
+++ frontend/interfaces/Gamepage.ts
@@ -1,34 +1,34 @@
-export interface Letter {
+export default interface Letter {
   id: string
   char: string
   used: boolean
   isExtra: boolean
 }
-export interface CategoryData {
-  word: string
-  valid: boolean
-  feedback: string
+export default interface CategoryData {
+  word: string;
+  valid: boolean;
+  feedback: string;
 }
-export interface StarData {
-  id: number
-  left: number
-  top: number
-  size: number
-  d: string
-  del: string
-  min: string
+export default interface StarData {
+  id: string;
+  left: number;
+  top: number;
+  size: number;
+  d: string;
+  del: string;
+  min: string;
 }
-export interface Category {
-  id: string
-  label: string
+export default interface Category {
+  id: string;
+  label: string;
 }
  
-export interface ValidateRequest {
-  word: string
-  category: string
-  letters: string[]
+export default interface ValidateRequest {
+  word: string;
+  category: string;
+  letters: string[];
 }
-export interface ValidateResponse {
-  isValid: boolean
-  message?: string
+export default interface ValidateResponse {
+  isValid: boolean;
+  message?: string;
 }
