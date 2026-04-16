5f4eb01e68116d6d6c352efe3a846be7edd418c0
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-16 09:32:52 +0200

    Back to shopPanel and type

--- frontend/pages/GamePage.tsx
+++ frontend/pages/GamePage.tsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import type { Letter, CategoryData, StarData, Category } from '../interfaces/Gamepage';
 import * as signalR from '@microsoft/signalr'
 import '../css/GamePage.css'
-import ShopPanel from './Shoppanel'
+import type ShopPanel from './ShopPanel'
 ///Star annimation
 const STATIC_STARS: StarData[] = Array.from({ length: 60 }).map((_, i) => ({
   id: i,
