d5ccd4f30768eb5715d63f0cc324727af51971f8
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-16 11:07:32 +0200

    Douple quate

--- frontend/pages/GamePage.tsx
+++ frontend/pages/GamePage.tsx
@@ -2,7 +2,7 @@
 import { useParams, useNavigate } from "react-router-dom";
 import type { Letter, CategoryData, StarData, Category } from '../interfaces/Gamepage';
 import * as signalR from '@microsoft/signalr'
-import '../css/GamePage.css'
+import "../css/GamePage.css"
 import ShopPanel from './Shoppanel'
 ///Star annimation
 const STATIC_STARS: StarData[] = Array.from({ length: 60 }).map((_, i) => ({
