7575618a8396d91ac54e3247d8bdba6cb4ac7c66
FatimaAlmurtadha <sukaina77150@gmail.com>
2026-04-17 15:48:49 +0200

    Proxy issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,8 +14,8 @@ export default defineConfig({
     proxy: {
       "/api": "http://127.0.0.1:5024",
       "/lobbyHub": {
-        target: "http://127.0.0.1:5024",
         ws: true,
+        target: "http://127.0.0.1:5024",
       },
     },
   },
