c714f78668838846cd7c81760042707554ccce03
alireza8850 <alireza.it101@gmail.com>
2026-04-17 12:45:39 +0200

    lobby fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,11 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://127.0.0.1:5024"
+      "/api": "http://127.0.0.1:5024",
+      "/lobbyHub": {
+        target: "http://127.0.0.1:5024",
+        ws: true,
+      },
     },
   },
 });
